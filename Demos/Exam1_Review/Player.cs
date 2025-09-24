c70d0a1fcec994b8e0e3cda0be2547f5e8e59959
Erika Mesh <esmvcs@rit.edu>
2025-09-24 12:10:44 -0400

    Wed 9/24 - Exam 1 review demo code

---
+++ Demos/Exam1_Review/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1_Review
+{
+    internal class Player
+    {
+        private string name;
+        private int id = 42;
+
+        public string Name
+        {
+            get
+            { 
+                return name;
+            }
+
+            set
+            {
+                if (value != null && value.Length > 0)
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new InvalidOperationException(
+                        "Can't set the name to a null or empty string");
+                }
+            }
+        }
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ Demos/Exam1_Review/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Exam1_Review
+{
+
+    // Write the code to declare an enum of type Platform
+    // with the following 3 states: stationary, floating and moving.
+    enum Platform
+    {
+        Stationary, 
+        Floating,
+        Moving
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ref types
+            Player a = new Player("Lacey");
+            Player b = a;
+            //a.Name = null;
+            a.Name = "Lacy";
+            Console.WriteLine(b.Name);
+
+            // Value types
+            int nA = 13;
+            int nB = nA;
+            nA++;
+            Console.WriteLine(nB);
+
+            DoStuff(a);
+
+            DoRecursiveThing(3);
+        }
+
+        public static int DoRecursiveThing(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return n * n + DoRecursiveThing(n - 1);
+            }
+        }
+
+
+        static void DoStuff(Player param)
+        {
+            Player localA = new Player("Shiro");
+            int nB = 13;
+        }
+    }
+}
