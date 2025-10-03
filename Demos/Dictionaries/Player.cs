4da1194df46a29848a694bcab1c9c626240e4f53
Erika Mesh <esmvcs@rit.edu>
2025-10-03 11:34:05 -0400

    Fri 10/3 - Dictionaries

---
+++ Demos/Dictionaries/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries
+{
+    internal class Player
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public Player(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+    }
+}
---
+++ Demos/Dictionaries/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<String, Player> playersDictionary =
+               new Dictionary<String, Player>();
+
+            // Create Player objects
+            Player pax = new Player("Pax the Dog", 200);
+            Player shiro = new Player("Shiro the Cat", 9872);
+
+            // Add via Add method
+            playersDictionary.Add("Pax", pax);
+
+            // Add via direct index
+            playersDictionary["Shiro"] = shiro;
+
+        }
+    }
+}
